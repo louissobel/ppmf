1cca4ea172e0a44a641705ed6294548c88da1f54
Louis Sobel <sobel@mit.edu>
2013-12-04 20:30:04 -0500

    rename core_encryption to encryption/aes.py

--- test/integration_test/encryption/aes_test.py
+++ test/integration_test/encryption/aes_test.py
@@ -1,12 +1,12 @@
 """
 basic check on a core_encryption roundtrip
 """
-import core_encryption
+from encryption import aes
 
 def test_roundtrip():
     message = "sdfkjl 3209oij \x03 sdlfkj \x34 dlfkj FOOBAR wekhj lalal pooop"
     password = 'poop'
-    assert message == core_encryption.decrypt(
-        core_encryption.encrypt(message, password),
+    assert message == aes.decrypt(
+        aes.encrypt(message, password),
         password
     )
